b60e41402885aa3517254e117a086d66f9334da1
devmaster5190 <devmaster5190@gmail.com>
2020-01-04 00:39:19 +0800

    added ui layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Header from "./components/layout/Header"
+import Content from "./components/layout/Content"
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <Content />
     </div>
   )
 }
